ad6dc27e99edb7595c24071882634241913297c0
CJ Cook <cookc@allegheny.edu>
2020-07-31 04:45:18 -0400

    Added new content to each section of website

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "chriscook81798@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
